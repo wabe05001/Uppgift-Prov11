316b79452e616d19afddb0aaeb149b8fa4bb46cd
leonkarlsson1 <Admin@TEB127032>
2023-02-17 10:25:17 +0100

    Add project files.

---
+++ Uppgift 1121/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Upgift._1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv din bas och höjd");
+            int höjd1 = int.Parse(Console.ReadLine());
+            int bas2 = int.Parse(Console.ReadLine());    
+            AREA(höjd1, bas2);
+            Console.WriteLine($"Arean är:{höjd1 * bas2} cm^2");
+
+        }
+        static int AREA(int höjd1, int bas2)
+        {
+            return (höjd1 * bas2);
+           
+         
+        }
+    }
+}
